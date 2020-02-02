5246c4b302e39a6a2beed8ad3f92d3f7218a83f5
Saad Khan <skhan8@mail.einstein.yu.edu>
2020-02-01 16:50:47 -0800

    Can code movement from an agent now

--- Assets/Scripts/AI/WalkingAgent.cs
+++ Assets/Scripts/AI/WalkingAgent.cs
@@ -13,7 +13,8 @@ public class WalkingAgent : Agent
     private States states; // This stores the GameObject's action state.
     private Rigidbody2D rBody; // We need a reference to this to perform kinematic operations on the GameObject.
     public Vector2 initial_position = new Vector2(0,0);
-
+    private float[] lastAction;
+    private int framecount = 0;
     private void Start()
     {
         states = GetComponent<States>();
@@ -22,6 +23,11 @@ public class WalkingAgent : Agent
 
     public Transform Target; // Stores the GameObject's position, orientation, and scale.
 
+    public override void InitializeAgent()
+    {
+        base.InitializeAgent();
+        AgentReset();
+    }
     public override void AgentReset()
     {
         Target.position = initial_position;
@@ -29,15 +35,33 @@ public class WalkingAgent : Agent
 
     public override void CollectObservations()
     {
+        print("hiii");
+        // Target and Agent positions
+        AddVectorObs(Target.position);
+        AddVectorObs(this.transform.position);
 
+        // Agent Velocity
+        AddVectorObs(rBody.velocity.x);
+        AddVectorObs(rBody.velocity.y);
+    }
+
+    public void ComputeReward()
+    {
+        float distanceToTarget = Vector2.Distance(
+            this.transform.position,
+            Target.position);
+
+        SetReward(Mathf.Min(10f, 10f / distanceToTarget));
     }
 
     public float speed = 3;
     public override void AgentAction(float[] vectorAction)
     {
-        print(vectorAction);
+
+        ComputeReward();
         states.horizontal_tilt = vectorAction[0];
         states.vertical_tilt = vectorAction[1];
+        print(states.horizontal_tilt);
         if (vectorAction[2] > .5)
         {
             states.jump_button_pressed = true;
@@ -57,13 +81,21 @@ public class WalkingAgent : Agent
     public override float[] Heuristic()
     {
         var action = new float[4];
-        action[0] = 1;  Random.Range(-1,1);
-        action[1] = 0;
-        action[2] = 0;//Random.Range(0,1);
-        action[3] = 0;
 
+        action[0] = Random.Range(0, 3)-1f;
+        action[1] = 3;
+        action[2] = Random.Range(0, 3)-1f;
+        action[3] = 0;
         return action;
     }
 
+    public void Update()
+    {
+        if (framecount % 20 == 0) {
+            lastAction = Heuristic();
+        } 
+        AgentAction(lastAction);
+        framecount++;
+    }
 }
 
